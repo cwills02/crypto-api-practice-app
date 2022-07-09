07255e05a1f2fda9e73cddba6cbda23cb11c4e40
Corey <Corey@Coreys-MBP.lan>
2022-07-09 17:07:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState, useEffect } from "react";
 
-function App() {
+// Use this API
+// https://api2.binance.com/api/v3/ticker/24hr
+
+// symbols we want...
+// BTCUSDT (Bitcoin)
+// ETHUSDT (Ethereum)
+// SOLUSDT (Solana)
+// ADAUSDT (Cardano)
+// DOGEUSDT (DogeCoin)
+
+const COIN_NAMES = {
+  BTCUSDT: "Bitcoin",
+  ETHUSDT: "Ethereum",
+  SOLUSDT: "Solana",
+  ADAUSDT: "Cardano",
+  DOGEUSDT: "DogeCoin"
+};
+
+const COINS = Object.keys(COIN_NAMES);
+
+export default function App() {
+  // 1. STATE AND USEEFFECT HERE
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api2.binance.com/api/v3/ticker/24hr")
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredData = data.filter((ticker) => {
+          if (COINS.includes(ticker.symbol)) {
+            return true;
+          }
+        });
+        setCoinData(filteredData);
+      });
+  }, []);
+
+  // 2. How will you "Pull out" the symbols we need?
+
+  // 3. ...and then store them in state?
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img
+          alt="logo"
+          src="https://assets.codepen.io/6060109/crypto-logo-secondary.png"
+        />
+        <input type="text" placeholder="Search" />
+      </nav>
+      <div className="main-content">
+        <h2>Today's cryptocurrency prices</h2>
+        <table>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h %</th>
+          </tr>
+          {/* 3. Display data here...  */}
+          {coinData.map((coin, idx) => {
+            return (
+              <tr key={coin.symbol}>
+                <td>{idx + 1}</td>
+                <td>{COIN_NAMES[coin.symbol]}</td>
+                <td>${Number(coin.lastPrice).toLocaleString()}</td>
+                <td
+                  style={
+                    Number(coin.priceChangePercent) > 0
+                      ? { color: "green" }
+                      : { color: "red" }
+                  }
+                >
+                  {Number(coin.priceChangePercent) > 0 ? "▲" : "▼"}
+                  {coin.priceChangePercent}%
+                </td>
+              </tr>
+            );
+          })}
+          {/* HINT: Map to JSX */}
+
+          {/* Up? Green + ▲ */}
+          {/* Down? Red + ▼ */}
+        </table>
+        <div className="bottom-logo-ctr">
+          <img
+            className="bottom-logo"
+            alt="logo"
+            src="https://assets.codepen.io/6060109/crypto-logo-primary.png"
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
